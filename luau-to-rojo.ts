679cd4464555887baefaaef962f192627e94422f
fox <83943819+fewkz@users.noreply.github.com>
2023-05-01 02:32:32 -0400

    Support changing root directory and rojo project name

--- luau-to-rojo.ts
+++ luau-to-rojo.ts
@@ -3,31 +3,47 @@ import {
   emptyDirSync,
   walkSync,
 } from "https://deno.land/std@0.160.0/fs/mod.ts";
-import { dirname } from "https://deno.land/std@0.160.0/path/mod.ts";
+import {
+  basename,
+  dirname,
+  relative,
+} from "https://deno.land/std@0.160.0/path/mod.ts";
 
 await new Command()
   .name("luau-to-rojo")
   .version("0.1.0")
   .description("Converts a Luau project to a Rojo project.")
-  .arguments("<projectPath:string> <outputPath:string>")
-  .action((_options, projectPath, outputPath) => {
-    // Expand output path to absolute path
-    projectPath = Deno.realPathSync(projectPath);
+  .arguments("<path:string> <outputPath:string>")
+  .option(
+    "-r, --root <rootPath:string>",
+    "The root path of the Luau project to resolve requires from",
+    { default: "." },
+  )
+  .option(
+    "-n, --name [name:string]",
+    "The name of the Rojo project, defaults to the root directory's name",
+  )
+  .action(({ root, name }, path, outputPath) => {
+    root = Deno.realPathSync(root);
+    path = Deno.realPathSync(path);
+    const pathRelativeToRoot = relative(root, path);
+
     emptyDirSync(outputPath);
     Deno.mkdirSync(outputPath + "/src", { recursive: true });
+
     // Convert outputPath to a RegExp that can be skipped
     // This code is magic spit out by copilot.
     const skipPath = new RegExp(
       outputPath.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&"),
     );
     for (
-      const entry of walkSync(projectPath, {
+      const entry of walkSync(path, {
         exts: [".lua", ".luau"],
         skip: [skipPath],
       })
     ) {
       if (!entry.isFile) continue;
-      const relativePath = entry.path.substring(projectPath.length);
+      const relativePath = entry.path.substring(path.length);
       // Find the depth of the path
       const depth = relativePath.split("/").length - 1;
       // if it's a directory, create it in the output directory
@@ -35,12 +51,20 @@ await new Command()
         recursive: true,
       });
       let text = Deno.readTextFileSync(entry.path);
+
       // Replace string-based require() calls with module-based require() calls.
       // We need to replace `/` with `.` to make it a valid module path.
       text = text.replace(
         /require\((.+)\)/g,
-        (_match, p1) => `require(${p1.replace(/\//g, ".")})`,
+        (_match, p1) => {
+          const normalizedPath = p1.replace(/\//g, ".").replace(
+            pathRelativeToRoot.replace(/\//g, ".") + ".",
+            "",
+          );
+          return `require(${normalizedPath})`;
+        },
       );
+
       // We need to repeat `.Parent` based on the depth of the path.
       text = text.replace(
         /require\("(.+)"\)/g,
@@ -51,7 +75,8 @@ await new Command()
         text,
       );
     }
-    const name = projectPath.split("/").pop();
+    if (!name) name = basename(root);
+
     const rojoProject = { name, tree: { $path: "src" } };
     Deno.writeTextFileSync(
       outputPath + "/default.project.json",
